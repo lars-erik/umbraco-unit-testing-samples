abcfbdcf0652e7a3ae6c6dcd097dd70f69a31f10
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2016-04-21 22:52:37 +0200

    Simple property editor test

---
+++ Umb.Testing.Web/App_Plugins/NamePlaceHolder/nameplaceholder.js
@@ -0,0 +1,33 @@
+﻿(function (angular) {
+
+    var app;
+
+    function NamePlaceHolderController(scope, editorState) {
+        scope.model.value = scope.model.value || { placeholder: "", text: "" };
+        scope.state = editorState;
+
+        scope.$watch("state.current.name", function(value) {
+            scope.model.value.placeholder = value || "";
+        });
+    }
+
+    function initModule() {
+
+        app = angular.module("testing", ["umbraco.services"]);
+
+        try {
+            var umbraco = angular.module("umbraco");
+            umbraco.requires.push("testing");
+        } catch(e) {
+            // swallow if in unit-tests
+        }
+    }
+
+    function register() {
+        app.controller("testing.nameplaceholder.controller", ["$scope", "editorState", NamePlaceHolderController]);
+    }
+
+    initModule();
+    register();
+
+}(angular));
---
+++ Umb.Testing.Web/App_Plugins/NamePlaceHolder/tests/nameplaceholder.tests.js
@@ -0,0 +1,58 @@
+﻿/// <reference path="../../../Umbraco/lib/angular/1.1.5/angular.js" />
+/// <reference path="../../../Umbraco/lib/angular/1.1.5/angular-mocks.js" />
+/// <reference path="../nameplaceholder.js" />
+(function() {
+
+    describe("nameplaceholder", function() {
+
+        var scope,
+            controllerFactory,
+            editorState = {
+                current: {}
+            };
+
+        beforeEach(function() {
+            angular.module("umbraco.services", []);
+        });
+
+        beforeEach(module("testing"));
+
+        beforeEach(inject(function ($controller, $rootScope) {
+            controllerFactory = $controller;
+            scope = $rootScope.$new();
+            scope.model = {};
+            editorState.current = {};
+        }));
+
+        function createController() {
+            controllerFactory("testing.nameplaceholder.controller", { $scope: scope, editorState: editorState });
+        }
+
+        it("initializes value if nothing", function () {
+            createController();
+            expect(scope.model.value).toEqual({
+                placeholder: "",
+                text: ""
+            });
+        });
+
+        it("sets placeholder to name of content", function () {
+            editorState.current.name = "another name";
+            createController();
+            scope.$digest();
+            expect(scope.model.value.placeholder).toBe("another name");
+        });
+
+        it("updates placeholder when name changes", function () {
+            createController();
+            scope.$digest();
+            expect(scope.model.value.placeholder).toBe("");
+
+            editorState.current.name = "another name";
+            scope.$digest();
+            expect(scope.model.value.placeholder).toBe("another name");
+        });
+
+    });
+
+}());
