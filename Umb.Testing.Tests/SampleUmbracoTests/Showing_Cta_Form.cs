16fd5e6f2f8299eca66585ac29e020695c2844be
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2016-06-07 21:35:04 +0200

    Test that calling CtaFormController.Form returns the specified view (failing)

---
+++ Umb.Testing.Tests/SampleUmbracoTests/Showing_Cta_Form.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Umb.Testing.Web.Controllers;
+
+namespace Umb.Testing.Tests.SampleUmbracoTests
+{
+    [TestFixture]
+    public class Showing_Cta_Form
+    {
+        [Test]
+        public void Should_Return_The_Specified_View()
+        {
+            const string expectedViewName = "AnonymousVisitor";
+            var controller = new CtaFormController();
+            var result = controller.Form(expectedViewName);
+            Assert.AreEqual(expectedViewName, result.ViewName);
+        }
+    }
+}
---
+++ Umb.Testing.Web/Controllers/CtaFormController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace Umb.Testing.Web.Controllers
+{
+    public class CtaFormController : SurfaceController
+    {
+        public PartialViewResult Form(string viewName)
+        {
+            return PartialView(viewName);
+        }
+    }
+}
