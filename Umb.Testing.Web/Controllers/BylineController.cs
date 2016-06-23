c9053469664a44ecb11a4ad45329c9ae22ede4cd
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2016-06-23 23:20:46 +0200

    Added test for surface controller demo on http://blog.aabech.no/archive/the-basics-of-unit-testing-umbraco/

---
+++ Umb.Testing.Web/Controllers/BylineController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Mvc;
+using Umb.Testing.Web.Models;
+using Umbraco.Core;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+
+namespace Umb.Testing.Web.Controllers
+{
+    public class BylineController : SurfaceController
+    {
+        public BylineController(UmbracoContext umbracoContext, UmbracoHelper umbracoHelper)
+            : base(umbracoContext, umbracoHelper)
+        {
+        }
+
+        public PartialViewResult Byline()
+        {
+            var content = Umbraco.AssignedContentItem;
+            return PartialView("Byline", new BylineModel(
+                content.GetPropertyValue<string>("author").IfNullOrWhiteSpace(content.WriterName), 
+                content.GetPropertyValue<DateTime?>("date") ?? content.CreateDate
+            ));
+        }
+    }
+}
---
+++ Umb.Testing.Web/Models/BylineModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Umb.Testing.Web.Models
+{
+    public class BylineModel
+    {
+        public string Author { get; }
+        public DateTime? Date { get; }
+
+        public BylineModel(string author, DateTime? date)
+        {
+            Author = author;
+            Date = date;
+        }
+    }
+}
