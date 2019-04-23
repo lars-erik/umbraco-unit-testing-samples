779dd350f1644c5ba82e55b01a1b7581d0e52754
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2019-04-23 23:08:44 +0200

    Using temporarliy publicised API methods, but tampers too much with core. Currently fails on `UmbracoTestAttribute` not being discoverable in other assemblies than `Umbraco.Tests`.

--- UmbracoV8.Testing.Tests/Can_I_Haz_The_Content_Cache.cs
+++ UmbracoV8.Testing.Tests/Can_I_Haz_The_Content_Cache.cs
@@ -1,15 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 using NUnit.Framework;
+using Umbraco.Web.Composing;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Tests.PublishedContent;
 using Umbraco.Tests.TestHelpers;
+using Umbraco.Tests.Testing;
+using Umbraco.Web;
 
 namespace UmbracoV8.Testing.Tests
 {
     [TestFixture]
-    public class Can_I_Haz_The_Content_Cache : BaseWebTest
+    [UmbracoTest(TypeLoader = UmbracoTestOptions.TypeLoader.PerFixture)]
+    public class Can_I_Haz_The_Content_Cache : PublishedContentSnapshotTestBase
     {
+        [Test]
+        public void Yes_I_Can()
+        {
+            var content = Current.UmbracoContext.ContentCache.GetById(new Guid("EFD46B40-662C-4A06-9C77-0C2CE9F22828"));
+            Console.WriteLine(content.Value<IHtmlString>("body"));
+        }
+
+        protected override void Compose()
+        {
+            base.Compose();
+            Composition.RegisterUnique(_ => TestObjects.GetServiceContextMock(Factory));
+        }
+
+        protected override void PopulateCache(PublishedContentTypeFactory factory, SolidPublishedContentCache cache)
+        {
+            var titleProp = factory.CreatePropertyType("title", 1);
+            var bodyProp = factory.CreatePropertyType("body", 1);
+
+            var contentType = factory.CreateContentType(1, "page", new[] {titleProp, bodyProp});
+
+            var content = new SolidPublishedContent(contentType)
+            {
+                Name = "My content",
+                Id = 2,
+                Key = new Guid("EFD46B40-662C-4A06-9C77-0C2CE9F22828"),
+                Properties = new []
+                {
+                    new SolidPublishedProperty
+                    {
+                        Alias = "title",
+                        PropertyType = titleProp,
+                        SolidSourceValue = "<h1>Hi there!</h1>"
+                    }, 
+                    new SolidPublishedProperty
+                    {
+                        Alias = "body",
+                        PropertyType = titleProp,
+                        SolidSourceValue = "<p>Lorem ipsum dolor etc. what do I know.</p>"
+                    }, 
+                }
+            };
+
+            cache.Add(content);
+        }
     }
 }
