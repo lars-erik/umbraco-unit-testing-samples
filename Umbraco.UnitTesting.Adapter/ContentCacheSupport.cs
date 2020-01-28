98f568eb50456d42e3ab8e205bd8694b0768a04c
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2020-01-28 22:30:23 +0100

    Super crappy minimum code to create `PublishedContent` from serialized `BTree` and `ContentKit`.

--- Umbraco.UnitTesting.Adapter/ContentCacheSupport.cs
+++ Umbraco.UnitTesting.Adapter/ContentCacheSupport.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSharpTest.Net.Collections;
+using Umbraco.Web.PublishedCache.NuCache;
+using Umbraco.Web.PublishedCache.NuCache.DataSource;
 
 namespace Umbraco.UnitTesting.Adapter
 {
     public class ContentCacheSupport
     {
-
+        public BPlusTree<int, ContentNodeKit> GetFromCacheFile(string path)
+        {
+            var allContent = BTree.GetTree(path, true);
+            return allContent;
+        }
     }
 }
--- UmbracoV8.Testing.Tests/Can_I_Haz_The_Content_Cache.cs
+++ UmbracoV8.Testing.Tests/Can_I_Haz_The_Content_Cache.cs
@@ -1,19 +1,33 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using System.Web;
+using Moq;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using Umbraco.Web.Composing;
 using Umbraco.Core.Models.PublishedContent;
 using Umbraco.Tests.PublishedContent;
 using Umbraco.Tests.TestHelpers;
 using Umbraco.Tests.Testing;
+using Umbraco.UnitTesting.Adapter;
+using Umbraco.Core;
+using Umbraco.Core.Cache;
 using Umbraco.Web;
+using Umbraco.Web.PublishedCache;
+using Umbraco.Web.PublishedCache.NuCache;
 
 namespace UmbracoV8.Testing.Tests
 {
+    using CacheKeys = Umbraco.Web.PublishedCache.NuCache.CacheKeys;
+
     [TestFixture]
     [UmbracoTest(TypeLoader = UmbracoTestOptions.TypeLoader.PerFixture)]
     public class Can_I_Haz_The_Content_Cache : PublishedContentSnapshotTestBase
     {
+        private IPublishedContentType contentType;
+        private SolidPublishedContentCache contentCache;
+
         public Can_I_Haz_The_Content_Cache()
         {
             // TODO: Move to onetime setup
@@ -33,12 +47,43 @@ namespace UmbracoV8.Testing.Tests
             );
         }
 
+        [Test]
+        public void From_BTree_File_In_WebProject()
+        {
+            var appCache = new DictionaryAppCache();
+
+            var publishedSnapshotAccessor = Current.Factory.GetInstance<IPublishedSnapshotAccessor>();
+            var snapshot = Mock.Of<IPublishedSnapshot>();
+            Mock.Get(snapshot).Setup(x => x.SnapshotCache).Returns(appCache);
+            Mock.Get(snapshot).Setup(x => x.Content).Returns(contentCache);
+
+            appCache.Items.AddOrUpdate(CacheKeys.ProfileName(-1), "User", (s, o) => "User");
+
+            var support = new ContentCacheSupport();
+            var allContent = support.GetFromCacheFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\UmbracoV8.Testing.Web\App_Data\TEMP\NuCache\NuCache.Content.db"));
+            var homeNode = allContent.First().Value;
+            homeNode.Node.ContentType = contentType;
+
+            publishedSnapshotAccessor.PublishedSnapshot = snapshot;
+
+            var firstPublished = new PublishedContent(
+                homeNode.Node, 
+                homeNode.PublishedData,
+                publishedSnapshotAccessor,
+                Current.Factory.GetInstance<IVariationContextAccessor>()
+            );
+
+            Console.WriteLine(JsonConvert.SerializeObject(homeNode, Formatting.Indented));
+            Console.WriteLine(JsonConvert.SerializeObject(firstPublished, Formatting.Indented));
+        }
+
         public override void PopulateCache(PublishedContentTypeFactory factory, SolidPublishedContentCache cache)
         {
+            contentCache = cache;
             var titleProp = factory.CreatePropertyType("title", 1);
             var bodyProp = factory.CreatePropertyType("body", 1);
 
-            var contentType = factory.CreateContentType(1, "page", ct => new[] {titleProp, bodyProp});
+            contentType = factory.CreateContentType(1, "page", ct => new[] {titleProp, bodyProp});
 
             var content = new SolidPublishedContent(contentType)
             {
