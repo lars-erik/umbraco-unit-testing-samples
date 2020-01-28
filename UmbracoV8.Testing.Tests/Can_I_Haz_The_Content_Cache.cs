bc9a746ecdb49525b3af6e280e6886935c1a903e
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2020-01-28 23:16:34 +0100

    #InfiniteScream while re-using classes. It's all private or local scope. :'(

--- UmbracoV8.Testing.Tests/Can_I_Haz_The_Content_Cache.cs
+++ UmbracoV8.Testing.Tests/Can_I_Haz_The_Content_Cache.cs
@@ -13,6 +13,9 @@ using Umbraco.Tests.Testing;
 using Umbraco.UnitTesting.Adapter;
 using Umbraco.Core;
 using Umbraco.Core.Cache;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Models;
+using Umbraco.Core.PropertyEditors;
 using Umbraco.Web;
 using Umbraco.Web.PublishedCache;
 using Umbraco.Web.PublishedCache.NuCache;
@@ -26,17 +29,43 @@ namespace UmbracoV8.Testing.Tests
     public class Can_I_Haz_The_Content_Cache : PublishedContentSnapshotTestBase
     {
         private IPublishedContentType contentType;
-        private SolidPublishedContentCache contentCache;
+        private PublishedContentTypeFactory publishedContentTypeFactory;
+        private IPublishedPropertyType titleProp;
+        private IPublishedPropertyType bodyProp;
+        private SolidPublishedContent solidContent;
+        private IPublishedSnapshot snapshot;
+        private DictionaryAppCache appCache;
+        private SolidPublishedContentCache fakeContentCache;
 
         public Can_I_Haz_The_Content_Cache()
         {
             // TODO: Move to onetime setup
             TestOptionAttributeBase.ScanAssemblies.Clear();
             TestOptionAttributeBase.ScanAssemblies.Add(GetType().Assembly);
+
+            var dataTypeService = new TestObjects.TestDataTypeService(
+                new DataType(new VoidEditor(Mock.Of<ILogger>())) { Id = 1 });
+
+            publishedContentTypeFactory = new PublishedContentTypeFactory(Mock.Of<IPublishedModelFactory>(), new PropertyValueConverterCollection(Array.Empty<IPropertyValueConverter>()), dataTypeService);
+            
+            appCache = new DictionaryAppCache();
+            fakeContentCache = new SolidPublishedContentCache();
+
+            snapshot = Mock.Of<IPublishedSnapshot>();
+            Mock.Get(snapshot).Setup(x => x.SnapshotCache).Returns(appCache);
+            Mock.Get(snapshot).Setup(x => x.Content).Returns(fakeContentCache);
+        }
+
+        public override void SetUp()
+        {
+            base.SetUp();
+
+            SetupContentType();
+            StubContentBySolidPublishedContent();
         }
 
         [Test]
-        public void Yes_I_Can()
+        public void From_Stubbed_SolidContent()
         {
             var content = Current.UmbracoContext.ContentCache.GetById(2);
 
@@ -48,23 +77,18 @@ namespace UmbracoV8.Testing.Tests
         }
 
         [Test]
-        public void From_BTree_File_In_WebProject()
+        public void From_Json_Serialized_Data_Via_The_NuCache_File()
         {
-            var appCache = new DictionaryAppCache();
 
             var publishedSnapshotAccessor = Current.Factory.GetInstance<IPublishedSnapshotAccessor>();
-            var snapshot = Mock.Of<IPublishedSnapshot>();
-            Mock.Get(snapshot).Setup(x => x.SnapshotCache).Returns(appCache);
-            Mock.Get(snapshot).Setup(x => x.Content).Returns(contentCache);
-
+            publishedSnapshotAccessor.PublishedSnapshot = snapshot;
+            
             appCache.Items.AddOrUpdate(CacheKeys.ProfileName(-1), "User", (s, o) => "User");
 
             var support = new ContentCacheSupport();
             var allContent = support.GetFromCacheFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\UmbracoV8.Testing.Web\App_Data\TEMP\NuCache\NuCache.Content.db"));
             var homeNode = allContent.First().Value;
             homeNode.Node.ContentType = contentType;
-
-            publishedSnapshotAccessor.PublishedSnapshot = snapshot;
 
             var firstPublished = new PublishedContent(
                 homeNode.Node, 
@@ -74,23 +98,18 @@ namespace UmbracoV8.Testing.Tests
             );
 
             Console.WriteLine(JsonConvert.SerializeObject(homeNode, Formatting.Indented));
+
             Console.WriteLine(JsonConvert.SerializeObject(firstPublished, Formatting.Indented));
         }
 
-        public override void PopulateCache(PublishedContentTypeFactory factory, SolidPublishedContentCache cache)
+        private void StubContentBySolidPublishedContent()
         {
-            contentCache = cache;
-            var titleProp = factory.CreatePropertyType("title", 1);
-            var bodyProp = factory.CreatePropertyType("body", 1);
-
-            contentType = factory.CreateContentType(1, "page", ct => new[] {titleProp, bodyProp});
-
-            var content = new SolidPublishedContent(contentType)
+            solidContent = new SolidPublishedContent(contentType)
             {
                 Name = "My content",
                 Id = 2,
                 Key = new Guid("EFD46B40-662C-4A06-9C77-0C2CE9F22828"),
-                Properties = new []
+                Properties = new[]
                 {
                     new SolidPublishedProperty
                     {
@@ -98,18 +117,32 @@ namespace UmbracoV8.Testing.Tests
                         PropertyType = titleProp,
                         SolidSourceValue = "<h1>Hi there!</h1>",
                         SolidValue = new HtmlString("<h1>Hi there!</h1>")
-                    }, 
+                    },
                     new SolidPublishedProperty
                     {
                         Alias = "body",
-                        PropertyType = titleProp,
+                        PropertyType = bodyProp,
                         SolidSourceValue = "<p>Lorem ipsum dolor etc. what do I know.</p>",
                         SolidValue = new HtmlString("<p>Lorem ipsum dolor etc. what do I know.</p>")
-                    }, 
+                    },
                 }
             };
 
-            cache.Add(content);
+            fakeContentCache.Add(solidContent);
+        }
+
+        private void SetupContentType()
+        {
+            titleProp = publishedContentTypeFactory.CreatePropertyType("title", 1);
+            bodyProp = publishedContentTypeFactory.CreatePropertyType("body", 1);
+
+            contentType = publishedContentTypeFactory.CreateContentType(1, "page", ct => new[] {titleProp, bodyProp});
+        }
+
+        public override void PopulateCache(PublishedContentTypeFactory publishedContentTypeFactory, SolidPublishedContentCache cache)
+        {
+            // We hijack all the stuff for now.
+            // Might climb the inheritance hierarchy and get rid of it
         }
     }
 }
