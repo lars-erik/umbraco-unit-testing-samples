524b28325ae04b6a5e23d1d16a7dd434fecae519
Lars-Erik <lea@markedspartner.no>
2021-07-19 14:55:35 +0200

    Can read BTree file as ContentNodeKits.

---
+++ Umbraco.UnitTesting.Adapter/ContentCacheSupport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CSharpTest.Net.Collections;
+using Umbraco.Cms.Core.Configuration.Models;
+using Umbraco.Cms.Infrastructure.PublishedCache;
+using Umbraco.Cms.Infrastructure.PublishedCache.DataSource;
+
+namespace Umbraco.UnitTesting.Adapter
+{
+    public class ContentCacheSupport
+    {
+        public BPlusTree<int, ContentNodeKit> GetFromCacheFile(string path)
+        {
+            var allContent = BTree.GetTree(path, true, new NuCacheSettings());
+            return allContent;
+        }
+    }
+}
---
+++ UmbracoV9.Testing.Tests/Can_I_Haz_Content_Cache.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Umbraco.UnitTesting.Adapter;
+
+namespace UmbracoV9.Testing.Tests
+{
+    [TestFixture]
+    public class Can_I_Haz_Content_Cache
+    {
+
+        [Test]
+        [Explicit]
+        public void Write_All_Content_Kits()
+        {
+            var allContentKits = new ContentCacheSupport().GetFromCacheFile("./../../../../UmbracoV9.Testing.Web/umbraco/Data/TEMP/NuCache/NuCache.Content.db");
+            Console.WriteLine(JsonConvert.SerializeObject(allContentKits.Select(x => new { x.Key, x.Value.ContentTypeId, x.Value.PublishedData.Name }), Formatting.Indented));
+        }
+    }
+}
