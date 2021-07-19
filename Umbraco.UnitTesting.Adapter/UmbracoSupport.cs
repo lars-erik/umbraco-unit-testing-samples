e5bde59ae5a2e2dece71cde90762e0c832f855d2
Lars-Erik <lea@markedspartner.no>
2021-07-19 15:33:10 +0200

    Wee look at changes from v8 - loads - need integration package

---
+++ Umbraco.UnitTesting.Adapter/UmbracoSupport.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CSharpTest.Net.Collections;
+using Moq;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.PropertyEditors;
+using Umbraco.Cms.Core.PublishedCache;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Scoping;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Infrastructure.PublishedCache;
+using Umbraco.Cms.Tests.Common;
+using Umbraco.Cms.Tests.Common.Testing;
+using Umbraco.Cms.Web.Common.UmbracoContext;
+using Current = Umbraco.Web.Composing.Current;
+
+namespace Umbraco.UnitTesting.Adapter
+{
+    using CacheKeys = Umbraco.Web.PublishedCache.NuCache.CacheKeys;
+
+    public class UmbracoSupport : UmbracoIntegrationTest
+    {
+        private readonly BPlusTree<int, ContentNodeKit> localDb;
+        private TestObjects.TestDataTypeService dataTypeService;
+        private PublishedContentTypeFactory publishedContentTypeFactory;
+        private IPublishedSnapshot snapshot;
+        private DictionaryAppCache appCache;
+        private IPublishedSnapshotAccessor snapshotAccessor;
+        private ContentStore contentStore;
+        private Action<Composition> compose;
+        private TestVariationContextAccessor testVariationContextAccessor;
+        private IPublishedSnapshotService snapshotService;
+        private List<IPublishedContentType> contentTypes = new List<IPublishedContentType>();
+
+        public UmbracoSupport(BPlusTree<int, ContentNodeKit> localDb = null)
+        {
+            this.localDb = localDb;
+
+            dataTypeService = new TestObjects.TestDataTypeService(
+                new DataType(new VoidEditor("", new DataValueEditorFactory())) { Id = 1 }
+            );
+
+            publishedContentTypeFactory = new PublishedContentTypeFactory(
+                Mock.Of<IPublishedModelFactory>(),
+                new PropertyValueConverterCollection(
+                    Array.Empty<IPropertyValueConverter>()
+                ),
+                dataTypeService
+            );
+        }
+
+        public static void RegisterForTesting<TFromAssembly>(TFromAssembly instance = null)
+            where TFromAssembly : class
+        {
+            var type = typeof(TFromAssembly);
+            var asm = type.Assembly;
+            if (TestOptionAttributeBase.ScanAssemblies.All(x => x != asm))
+            {
+                TestOptionAttributeBase.ScanAssemblies.Add(asm);
+            }
+        }
+
+        public new UmbracoContext GetUmbracoContext(string url, int templateId = 1234, RouteData routeData = null, bool setSingleton = false, IUmbracoSettingsSection umbracoSettings = null, IEnumerable<IUrlProvider> urlProviders = null, IEnumerable<IMediaUrlProvider> mediaUrlProviders = null, IGlobalSettings globalSettings = null, IPublishedSnapshotService snapshotService = null)
+        {
+            var service = snapshotService ?? this.snapshotService;
+            var httpContext = GetHttpContextFactory(url, routeData).HttpContext;
+
+            var umbracoContext = new UmbracoContext(
+                httpContext,
+                service,
+                new WebSecurity(httpContext, Factory.GetInstance<IUserService>(),
+                    Factory.GetInstance<IGlobalSettings>()),
+                umbracoSettings ?? Factory.GetInstance<IUmbracoSettingsSection>(),
+                urlProviders ?? Enumerable.Empty<IUrlProvider>(),
+                mediaUrlProviders ?? Enumerable.Empty<IMediaUrlProvider>(),
+                globalSettings ?? Factory.GetInstance<IGlobalSettings>(),
+                new TestVariationContextAccessor());
+
+            if (setSingleton)
+                Umbraco.Web.Composing.Current.UmbracoContextAccessor.UmbracoContext = umbracoContext;
+
+            return umbracoContext;
+        }
+
+        /// <summary>
+        /// Initializes a stubbed Umbraco request context. Generally called from [SetUp] methods.
+        /// Remember to call UmbracoSupport.DisposeUmbraco from your [TearDown].
+        /// </summary>
+        public void SetupUmbraco(Action<Composition> compose = null)
+        {
+            this.compose = compose;
+
+            appCache = new DictionaryAppCache();
+            appCache.Items.AddOrUpdate(CacheKeys.ProfileName(-1), "User", (s, o) => "User");
+
+            //RegisterForTesting(this);
+            base.SetUp();
+        }
+
+        protected override void Compose()
+        {
+            base.Compose();
+
+            compose?.Invoke(Composition);
+        }
+
+        protected override IPublishedSnapshotService CreatePublishedSnapshotService()
+        {
+            // Base sets private ContentTypesCache
+            base.CreatePublishedSnapshotService();
+
+            snapshotService = Mock.Of<IPublishedSnapshotService>();
+            snapshotAccessor = Mock.Of<IPublishedSnapshotAccessor>();
+            snapshot = Mock.Of<IPublishedSnapshot>();
+            snapshotAccessor.PublishedSnapshot = snapshot;
+
+            testVariationContextAccessor = new TestVariationContextAccessor();
+
+            contentStore = new ContentStore(
+                snapshotAccessor,
+                testVariationContextAccessor,
+                Current.Factory.GetInstance<ILogger>(),
+                localDb
+            );
+
+            using (contentStore.GetScopedWriteLock(Current.Factory.GetInstance<IScopeProvider>()))
+            {
+                contentStore.SetAllContentTypesLocked(contentTypes);
+                contentStore.SetAllLocked(localDb.Values);
+            }
+
+            var storeSnapshot = contentStore.CreateSnapshot();
+            //new ContentStore.Snapshot(contentStore, 1, Current.Logger);
+
+            var anotherCache = new ContentCache(
+                false,
+                storeSnapshot,
+                appCache,
+                null,
+                Mock.Of<IDomainCache>(),
+                Current.Configs.Global(),
+                Current.VariationContextAccessor
+            );
+
+            Mock.Get(snapshot).Setup(x => x.SnapshotCache).Returns(appCache);
+            Mock.Get(snapshot).Setup(x => x.Content).Returns(anotherCache);
+
+            Mock.Get(snapshotService).Setup(x => x.CreatePublishedSnapshot(It.IsAny<string>())).Returns(() =>
+            {
+                return snapshot;
+            });
+
+            return snapshotService;
+        }
+
+        /// <summary>
+        /// Cleans up the stubbed Umbraco request context. Generally called from [TearDown] methods.
+        /// Must be called before another UmbracoSupport.SetupUmbraco.
+        /// </summary>
+        public void DisposeUmbraco()
+        {
+            base.TearDown();
+        }
+
+        public IPublishedPropertyType CreatePropertyType(string alias, int type)
+        {
+            return publishedContentTypeFactory.CreatePropertyType(alias, type);
+        }
+
+        public void CreateContentType(int id, string alias,
+            Func<IPublishedContentType, IEnumerable<IPublishedPropertyType>> propertyFactory)
+        {
+            var publishedContentType = publishedContentTypeFactory.CreateContentType(Guid.NewGuid(), id, alias, propertyFactory);
+            contentTypes.Add(publishedContentType);
+        }
+    }
+}
